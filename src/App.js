8cab1c1e53661c62c58250d1fa4bc2af6e24eb30
prakhar2806 <prakhar.ngm92@gmail.com>
2023-03-08 23:12:24 +0530

    add app changes as well

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // Import everything needed to use the `useQuery` hook
-import React from "react";
+import React, { useState } from "react";
 import About from "./components/About";
 import Cart from "./components/Cart";
 import Home from "./components/Home";
@@ -7,8 +7,11 @@ import Wishlist from "./components/Wishlist";
 import Profile from "./components/Profile";
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.scss";
+import GoogleUserLogin from "./components/GoogleUserLogin";
 
 export default function App() {
+  const [loginSuccess, setLoginSuccess] = useState(false);
+
   return (
     <div>
       <div className="header">
@@ -24,13 +27,18 @@ export default function App() {
           </Link>
           <Link to="userProfile">User Profile</Link>
           <Link to="about">About</Link>
+          {/* <GoogleLogin onSuccess={responseMessage} onError={errorMessage} /> */}
+          <GoogleUserLogin loginSuccess={(val) => setLoginSuccess(val)} />
         </div>
       </div>
       <div className="content">
         <div className="contentWrapper">
           <div className="App">
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route
+                path="/"
+                element={loginSuccess ? <Home /> : <h2>Please Login</h2>}
+              />
               <Route path="cart" element={<Cart />} />
               <Route path="wishlist" element={<Wishlist />} />
               <Route path="userProfile" element={<Profile />} />
