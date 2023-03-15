300cfdb558d37dcef4861bf3daa56f0d0cdbe042
prakhar2806 <prakhar.ngm92@gmail.com>
2023-03-15 23:15:01 +0530

    login correction

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ export default function App() {
               <Route
                 path="/"
                 element={
-                  !loginSuccess ? (
+                  loginSuccess ? (
                     <Home />
                   ) : (
                     <>
