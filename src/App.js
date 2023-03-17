671c02bc6ecbe0509c6c311f78d64bc03c9800d6
prakhar2806 <prakhar.ngm92@gmail.com>
2023-03-18 02:52:26 +0530

    under maintenance

--- src/App.js
+++ src/App.js
@@ -16,70 +16,72 @@ export default function App() {
   const [loginSuccess, setLoginSuccess] = useState(false);
   const [profileData, setProfileData] = useState(null);
 
-  return (
-    <div>
-      <div className="header">
-        <Link className="logo" to="/">
-          Mahima's Shopping App
-        </Link>
-        <div className="header-right">
-          {!loginSuccess && !profileData && (
-            <Link className="active" to="userProfile">
-              Profile
-            </Link>
-          )}
-          {/* <Link to="about">About</Link> */}
-          <Link className="active" to="cart">
-            CART
-          </Link>
-          <Link className="active" to="wishlist">
-            WISHLIST
-          </Link>
-          {
-            <GoogleUserLogin
-              loginSuccess={(val) => setLoginSuccess(val)}
-              setProfileData={(data) => setProfileData(data)}
-            />
-          }
-          {loginSuccess && profileData && (
-            <span>
-              <img
-                className="profile-picture"
-                src={profileData.picture}
-                alt="user image"
-              />
-            </span>
-          )}
-        </div>
-      </div>
+  return <div> App under Maintenance</div>;
 
-      <div className="content">
-        <div className="contentWrapper">
-          <div className="App">
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  loginSuccess ? (
-                    <Home />
-                  ) : (
-                    <>
-                      <h2>Please Login</h2>
-                      <Celebration />
-                    </>
-                  )
-                }
-              />
-              <Route path="cart" element={<Cart />} />
-              <Route path="wishlist" element={<Wishlist />} />
-              <Route path="userProfile" element={<Profile />} />
-              <Route path="about" element={<About />} />
-              <Route path="/cart/checkout" element={<Checkout />} />
-              <Route path={"/product/item/:id"} element={<DetailsPage />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  //   return (
+  //     <div>
+  //       <div className="header">
+  //         <Link className="logo" to="/">
+  //           Shopping App
+  //         </Link>
+  //         <div className="header-right">
+  //           {!loginSuccess && !profileData && (
+  //             <Link className="active" to="userProfile">
+  //               Profile
+  //             </Link>
+  //           )}
+  //           {/* <Link to="about">About</Link> */}
+  //           <Link className="active" to="cart">
+  //             CART
+  //           </Link>
+  //           <Link className="active" to="wishlist">
+  //             WISHLIST
+  //           </Link>
+  //           {
+  //             <GoogleUserLogin
+  //               loginSuccess={(val) => setLoginSuccess(val)}
+  //               setProfileData={(data) => setProfileData(data)}
+  //             />
+  //           }
+  //           {loginSuccess && profileData && (
+  //             <span>
+  //               <img
+  //                 className="profile-picture"
+  //                 src={profileData.picture}
+  //                 alt="user image"
+  //               />
+  //             </span>
+  //           )}
+  //         </div>
+  //       </div>
+
+  //       <div className="content">
+  //         <div className="contentWrapper">
+  //           <div className="App">
+  //             <Routes>
+  //               <Route
+  //                 path="/"
+  //                 element={
+  //                   loginSuccess ? (
+  //                     <Home />
+  //                   ) : (
+  //                     <>
+  //                       <h2>Please Login</h2>
+  //                       <Celebration />
+  //                     </>
+  //                   )
+  //                 }
+  //               />
+  //               <Route path="cart" element={<Cart />} />
+  //               <Route path="wishlist" element={<Wishlist />} />
+  //               <Route path="userProfile" element={<Profile />} />
+  //               <Route path="about" element={<About />} />
+  //               <Route path="/cart/checkout" element={<Checkout />} />
+  //               <Route path={"/product/item/:id"} element={<DetailsPage />} />
+  //             </Routes>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
 }
